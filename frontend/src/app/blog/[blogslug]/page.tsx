12933a967e72f2f1e6586bf9dd8b8f1338b948ef
odhiambokevin <kevinabrahamsodhiambo@gmail.com>
2025-10-31 12:30:53 +0300

    chore: add django backend

--- frontend/src/app/blog/[blogslug]/page.tsx
+++ frontend/src/app/blog/[blogslug]/page.tsx
@@ -137,9 +137,9 @@ export default async function Page({
     <div className="w-full px-4 lg:w-4/12">
       <div className="mb-10 mt-12 rounded-sm bg-text-mild/20 p-6 shadow-three lg:mt-0">
                <div className="relative w-3/4 mx-auto">
-                <form className={`relative flex items-center gap-[8px] bg-gray-400 p-[4px] rounded-[8px] outline outline-secondary`}>
+                <form className={`relative flex items-center gap-[8px] bg-gray-400/90 p-[4px] rounded-[8px] `}>
                     <input type="text" placeholder="Search" className="bg-transparent px-1 text-white outline-0"/>
-                    <button className="absolute top-0 right-0 bottom-0 bg-secondary rounded-md rounded-bl-none rounded-tl-none px-2 py-[px] cursor-pointer">
+                    <button className="absolute top-0 right-0 bottom-0 bg-accent rounded-md rounded-bl-none rounded-tl-none px-2 py-[px] cursor-pointer">
                     <MdSearch className="text-2xl "/>
                     </button>
                 </form>
@@ -150,13 +150,13 @@ export default async function Page({
                   Related Posts
                 </h3>
                 <div className="flex flex-col p-8 text-text-mild">
-                  <Link href="#" className="mb-6 border-b border-body-color border-opacity-10 pb-6">
+                  <Link href="#" className="hover:text-accent mb-6 border-b border-body-color border-opacity-10 pb-6">
                     Django is cool
                   </Link >
-                  <Link href="#" className="mb-6 border-b border-body-color border-opacity-10 pb-6">
+                  <Link href="#" className="hover:text-accent mb-6 border-b border-body-color border-opacity-10 pb-6">
                     Storing passwords in databases
                   </Link>
-                  <Link href="#">
+                  <Link href="#" className="hover:text-accent mb-6 border-b border-body-color border-opacity-10 pb-6">
                     Managing websockets
                   </Link>
                 </div>
