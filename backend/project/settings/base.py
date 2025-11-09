1da9e9e942361ccc71ad0b342975894c6ccffaf5
odhiambokevin <kevinabrahamsodhiambo@gmail.com>
2025-11-09 20:22:10 +0300

    refactor: edit static config

--- backend/project/settings/base.py
+++ backend/project/settings/base.py
@@ -135,7 +135,7 @@ USE_TZ = True
 
 # Static files
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
 
 
 # Media files
