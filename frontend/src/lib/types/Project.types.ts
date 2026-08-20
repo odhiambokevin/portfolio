d59cf888760673eeea022e33d67ab12a35915824
odhiambokevin <odhiambosiaya@proton.me>
2026-08-20 18:24:56 +0300

    add project data

--- frontend/src/lib/types/Project.types.ts
+++ frontend/src/lib/types/Project.types.ts
@@ -1,15 +1,6 @@
 export type ProjectType = {
-    id:number,
+    id:number, 
     title: string
-    subtitle: string
-    slug: string
-    author: string
-    time: string
-    size: string
-    credit: string
-    image: string
-    date: string
-    description: string
     stack: string[]
     url: string
 }
