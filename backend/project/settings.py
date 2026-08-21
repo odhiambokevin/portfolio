1c8acc70feb8f21cd48133f4ccd3a01d18078286
odhiambokevin <odhiambosiaya@proton.me>
2026-08-21 14:57:02 +0300

    fix: update static file dirs

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -146,7 +146,7 @@ USE_TZ = True
 #static files (CSS, JavaScript, Images)
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
-STATICFILES_DIRS = [BASE_DIR / "static",]
+STATICFILES_DIRS = os.path.join(BASE_DIR / "static",)
 
 #media files
 MEDIA_URL = "/media/"
