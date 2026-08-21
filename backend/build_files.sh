93ab19c63a84d0acdeb4706df63a24493b129658
odhiambokevin <odhiambosiaya@proton.me>
2026-08-21 11:48:22 +0300

    config: edit build script

--- backend/build_files.sh
+++ backend/build_files.sh
@@ -1,5 +1,5 @@
 # build_files.sh
-pip install -r requirements.txt  --break-system-packages
+pip install -r requirements.txt --break-system-packages
 
 # make migrations
 python3 manage.py collectstatic --no-input
